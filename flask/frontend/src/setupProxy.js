2ad05ce39ea8c5ca7c8edfa695c2796da0a34815
pepperumo <pepperumo@gmail.com>
2025-03-18 05:29:19 +0100

    mcp creation and deletion

--- flask/frontend/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app) {
-  app.use(
-    '/api',
-    require('http-proxy-middleware').createProxyMiddleware({
-      target: 'http://localhost:5000',
-      changeOrigin: true,
-    })
-  );
-};
